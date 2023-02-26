076f4da0164554c19fde57d0e14b73ebe77b4627
Shafiqullah Türkmen <shafiqullah.turkmen@alo-tech.com>
2023-02-26 16:50:09 +0300

    Notes

---
+++ src/main/java/com/alparslan/tutorials/JavaTutorials.java
@@ -0,0 +1,64 @@
+package com.alparslan.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+    public static void main(String[] args) {
+
+        // Arrays.sort();
+        // this
+        // super
+        // break continue return
+        // optional
+        // cast
+        // primitive type
+        // wrapper class
+        // Random
+        // Math
+        // ½s ½d ½f ==> printf()
+        // final
+        // static
+        // decoder encoder
+        // intanceof
+        // public private protected
+        // String
+        // StringBuilder
+        // StringTokenizer
+        // StringBuffer
+        // Enum
+        // Multithread
+
+        // S.O.L.I.D
+        // KISS
+        // DRY
+        // MONAD
+        // List Set Map
+        // Liste: LinkedList ArrayList
+        // Generic <>
+        // Iterator
+        // interface
+        // abstract new
+        // inheritance
+
+        // UML - + __
+        // class
+        // POJO
+        // Bean
+        // Serileştirme
+        // IO
+        // JAR WAR
+        // Dağıtık
+        // server
+
+        // constructor
+        // exception
+        // error
+
+        // Java 8 : stream, map, filter, sort
+        // Optional
+        // Lambda Expression
+        // Method Expression
+
+        // D.P.   Design pattern
+    }
+}
