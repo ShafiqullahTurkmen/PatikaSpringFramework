c5800fe52be0823ef67893fa44fa6bb0a3c2f2cc
Shafiqullah Türkmen <shafiqullah.turkmen@alo-tech.com>
2023-02-26 18:24:29 +0300

    CDI @Produces added

---
+++ src/main/java/com/alparslan/cdi/_02_Produces.java
@@ -0,0 +1,22 @@
+package com.alparslan.cdi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value = "producetuto")
+@ApplicationScoped
+public class _02_Produces {
+    @Produces // Üretiyor
+    public List<String> getList() {
+        List<String> liste = new ArrayList<>();
+        liste.add("Html5");
+        liste.add("css3");
+        liste.add("js");
+        liste.add("react");
+        liste.add("angular");
+        return liste;
+    }
+}
